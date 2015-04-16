90475b7df31b586841d8ea6f11de3ef8ec655763
Ankit Gupta <ag7bf@virginia.edu>
2015-04-16 00:36:27 -0400

    Wednesday night updates

--- practice.py
+++ practice.py
@@ -1,6 +1,10 @@
 from blockchain import blockexplorer
+from decimal import *
 import requests
+import operator
 import datetime
+import hashlib
+# import util
 
 print '\n\n\n\n\n\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n'
 
@@ -44,37 +48,149 @@ def get_transaction_amount(tx):
 # block_hash = '000000000000000002b32e242989056214fef31c5aac08ae517840db3e3e7fd2'
 # block_num = '341537'
 # block = blockexplorer.get_block(block_hash)
+def totalValue(xPuts):
+	totalValue = 0
+	for x in xPuts:
+		totalValue += x.value
+	return totalValue
+
+def contains(address_string, array):
+	for xPut in array:
+		if address_string == xPut.address:
+			return True
+	return False
+
+def createLeavingEdges(edge_factory, edges, address_string, inputs, outputs):
+	total_output = totalValue(outputs)
+	# print total_output
+	for i in inputs:
+		if address_string == i.address:
+			value = i.value
+			
+			for o in outputs:
+				received = o.value
+				ratio = Decimal(received) / Decimal(total_output)
+				weighted_received = ratio *  Decimal(value)
+				e = edge_factory.NewEdge(address_string, o.address, weighted_received)
+				edges.append(e)
+
+def createIncomingEdges(edge_factory, edges, address_string, inputs, outputs):
+	total_output = totalValue(outputs)
+	for o in outputs:
+		if address_string == o.address:
+			received = o.value
+			ratio = Decimal(received) / Decimal(total_output)
+			for i in inputs:
+				weighted_sent = ratio * Decimal(i.value)
+				e = edge_factory.NewEdge(i.address, address_string, weighted_sent)
+				edges.append(e)
+
+
+
+
 
 class Edge: 
+	def __init__(self, s, t, w, si, ti):
+		self.source = s
+		self.target = t
+		self.weight = w
+		self.source_index = si
+		self.target_index = ti
+
+	def __repr__(self):
+		s = '[ \'' + self.source + '\', ' + '\'' + self.target + '\', ' + str(self.weight) + ']'
+		
+		return s
+
+class EdgeFactory:
 	def __init__(self):
-		self.source = ''
-		self.target = ''
-		self.weight = 0
+		self.node_counter = 0
+		self.node_index_map = {}
+
+	def NewEdge(self, source, target, weight):
+		self.node_index_map
+		if source in self.node_index_map:
+			source_index = self.node_index_map[source]
+		else:
+			self.node_index_map[source] = self.node_counter
+			source_index = self.node_counter
+			self.node_counter += 1
+
+		if target in self.node_index_map:
+			target_index = self.node_index_map[target]
+		else:
+			self.node_index_map[target] = self.node_counter
+			target_index = self.node_counter
+			self.node_counter += 1
+
+		edge = Edge(source, target, weight, source_index, target_index)
+		return edge
+
+	def GetNodeIndexMap(self):
+		return self.node_index_map
+
 
 
 
 unexploredAddresses = []
 exploredAddresses = []
+edges = []
+edge_factory = EdgeFactory()
+
+address_string = '17bWK1kGySckEV7jpNBHUmHuX1UoHeHoAs'
+address_object = blockexplorer.get_address(address_string)
+
+# attributes = dir(address)
+# print 'Address attributes:'
+# for t in attributes:
+# 	x = getattr(address,t)
+
+# 	print '\t',t, '\t\t\t', x
+
+# outputtemp = address_object.transactions[0].outputs[0]
+# attributes = dir(outputtemp)
+# print 'Output attributes:'
+# for t in attributes:
+# 	x = getattr(outputtemp,t)
+
+# 	print '\t',t, '\t\t\t', x
+
+i = 0
+transactions = address_object.transactions
+for t in transactions:
+	inputs = t.inputs
+	outputs = t.outputs
+	createLeavingEdges(edge_factory, edges, address_string, inputs, outputs)
+	createIncomingEdges(edge_factory, edges,address_string,inputs,outputs)
+	
+
+node_list_result = ''
+node_map = edge_factory.GetNodeIndexMap()
+node_list = node_map.items()
+node_list = sorted(node_list, key=operator.itemgetter(1))
+print node_list
+for node in node_list:
+	node_list_result += '{"name":"' + node[0] + '"},'
+node_list_result = node_list_result[:-1]
+
+link_list_result = ''
 
 
-address = '17bWK1kGySckEV7jpNBHUmHuX1UoHeHoAs'
-addrObj = blockexplorer.get_address(address)
 
-attributes = dir(addrObj)
-print 'Address attributes:'
-for t in attributes:
-	x = getattr(addrObj,t)
+result = '{"nodes":[' + node_list_result + '],"links":[' + link_list_result + ']}'
+print result
 
-	print '\t',t, '\t\t\t', x
-
-print 'transaction count:', len(addrObj.transactions)
+# for e in edges:
+# 	print str(e) + ','
 
 
 
+# {"nodes":[{"name":"Oil"},{"name":"Natural Gas"},{"name":"Coal"},{"name":"Fossil Fuels"},{"name":"Electricity"},{"name":"Energy"}],"links":[{"source":0,"target":3,"value":15},{"source":1,"target":3,"value":20},{"source":2,"target":3,"value":25},{"source":2,"target":4,"value":25},{"source":3,"target":5,"value":60},{"source":4,"target":5,"value":25},{"source":4,"target":5,"value":25},{"source":4,"target":4,"value":5.5}]}
 
 
 
-
+# hash = '62e907b15cbf27d5425399ebf6f0fb50ebb88f18'
+# print util.hash_to_address("00".decode('hex'), hash.decode('hex'))
 
 
 
